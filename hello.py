1b452899de554187599996fe52a17f3ca476d79a
Alexandre <alexandre.Dareau@gmail.com>
2021-04-22 19:23:14 +0200

    test

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 # Very fancy (and polite) piece of software
 
 print("Hello !!!")
+
+# now let's test black
+a = {1 : 'a first string', 2 : "a second string", 3: "a third string", 4:"bla bla bla bla black"}
