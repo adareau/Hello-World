9112d68e525ff7ef6668a3d6c4b2e4e576d5ce24
Alexandre <alexandre.Dareau@gmail.com>
2021-04-22 19:37:20 +0200

    testing pre-commit

--- hello.py
+++ hello.py
@@ -3,4 +3,9 @@
 print("Hello !!!")
 
 # now let's test black
-a = {1 : 'a first string', 2 : "a second string", 3: "a third string", 4:"bla bla bla bla black"}
+a = {
+    1: "a first string",
+    2: "a second string",
+    3: "a third string",
+    4: "bla bla bla bla black",
+}
